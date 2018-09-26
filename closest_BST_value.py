6df3502dcb0ddd5e3c39995377af4e36b4302205
Sherlock-szx <sherlockatszx@qq.com>
2018-09-26 10:06:48 +0800

    add dfs

--- closest_BST_value.py
+++ closest_BST_value.py
@@ -16,36 +16,87 @@ class Solution:
         # write your code here
         if root is None:
             return
-        self.compare =abs(target-root.val)
-        self.match_node ='infs'
+        #self.compare =abs(target-root.val)
+        self.match_node =root.val
         self.compareValue(root,target)
 
         return self.match_node
 
 
     def compareValue(self,node,target):
-        if node is None:
+        if node is None :
+
             return
         print(node.val)
-        if abs(self.compare) >= abs(node.val-target):
+        if abs(target-self.match_node) >= abs(node.val-target):
             self.match_node = node.val
-            self.compare = target-node.val
+            #self.compare = target-node.val
 
 
 
-            if self.compare<0:
-                self.compareValue(node.left,target)
+        #the below if condition shoule be the same level of the above 1
+        if target<node.val and node.left:
+            self.compareValue(node.left,target)
+            if  abs(node.left.val-target)<abs(self.match_node-target) :
+                self.match_node=node.left
 
 
 
-            if self.compare>0:
-                self.compareValue(node.right,target)
-
-        else:
-            if node.right and abs(node.right.val-target)>=abs(self.compare) \
-            and abs(node.right.val-target)<=abs(node.val-target):
-                self.match_node = node.right.val
-                self.compare = target - node.right.val
 
 
-'''this version ,{10,5,15,3,6,12,18,#,4,#,8} doesn't pass'''
+        if target>node.val and node.right:
+            print('in right')
+            self.compareValue(node.right,target)
+            if abs(node.right.val-target)< abs(self.match_node-target):
+                self.match_node=node.right
+
+'''log summary:1.not only compare with the current node,but also consider
+current_node.left or current_node.right, 2,self.match_node as a global variable
+to store the current node,3,the last 2 if condition should not be indent to the
+above one '''
+
+
+class Solution2:
+    """
+    @param root: the given BST
+    @param target: the given target
+    @return: the value in the BST that is closest to the target
+    """
+    def closestValue(self, root, target):
+        if not root:
+            raise Exception
+
+        lower = self.findLower(root, target)
+        upper = self.findUpper(root, target)
+        if lower and upper:
+            if target - lower.val > upper.val - target:
+                return upper.val
+            else:
+                return lower.val
+        if lower:
+            return lower.val
+        if upper:
+            return upper.val
+
+
+    def findLower(self, root, target):
+        curr = root
+        last = None
+        while curr:
+            if curr.val <= target:
+                last = curr
+                curr = curr.right
+            else:
+                curr = curr.left
+        return last  # may be None if no one <= target
+
+    def findUpper(self, root, target):
+        curr = root
+        last = None
+        while curr:
+            if curr.val > target:
+                last = curr
+                curr = curr.left
+            else:
+                curr = curr.right
+        return last  # may be None if no one > target
