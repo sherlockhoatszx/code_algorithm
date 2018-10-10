dbef3c6e40e4c6a28c1193e920dcaefb8812a2c2
Sherlock-szx <sherlockatszx@qq.com>
2018-10-10 15:36:34 +0800

    deep copy
    class solution 3 show the effect of the deepcopy

--- subsets.py
+++ subsets.py
@@ -12,9 +12,9 @@ class Solution:
         print(S)
         print(index)
 
-
+        #self.search(nums, S, index + 1)
         self.search(nums, S + [nums[index]], index + 1)
-        print('remove')
+        print('right child')
         self.search(nums, S, index + 1)
 
     def subsets(self, nums):
@@ -23,6 +23,7 @@ class Solution:
         return self.results
 import copy
 class Solution2:
+    '''choose subtree''''
     def subsets(self,nums):
         self.rets=[]
         def dfs(nums,temp,i):
@@ -38,11 +39,35 @@ class Solution2:
                 dfs(nums,temp,i+1)
                 print('pop')
                 temp.pop()
-                print('after pop',temp)
+                print('after pop',temp)#backtracking
         dfs(nums,[],0)
         return self.rets
 
-class solution3:
+
+class Solution3:
+    '''choose subtree，based on Solution2, use deepcopy in the for loop,then
+    the pop will not be needed''''
+    def subsets(self,nums):
+        self.rets=[]
+        def dfs(nums,temp,i):
+            print(i)
+            print('temp list is',temp)
+            #temp=copy.copy(temp)
+            self.rets.append(temp)
+            print('rets',self.rets)
+            for i in range(i,len(nums)):
+                print('internal i is',i)
+                print('appending to tmp is',nums[i])
+                newset = copy.deepcopy(temp)
+                newset.append(nums[i])
+                dfs(nums,newset,i+1)
+                #print('pop')
+                #temp.pop()
+                #print('after pop',temp)#backtracking
+        dfs(nums,[],0)
+        return self.rets
+
+class solution4:
     def subsets(self,nums):
         rets=[]
         self.dfs(nums,0,[],rets)
@@ -52,7 +77,7 @@ class solution3:
         print(rets)
         for i in range(index,len(nums)):
             self.dfs(nums,i+1,path+[nums[i]],res)
-
+#bfs
 s = Solution()
 nums=[1,2,3]
 
