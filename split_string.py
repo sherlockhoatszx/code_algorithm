232dc59c9e1a1dd6e7e4e26eeb18179da5d53385
Sherlock-szx <sherlockatszx@qq.com>
2018-10-12 15:15:33 +0800

    pass leetcode
    pass

--- split_string.py
+++ split_string.py
@@ -5,24 +5,38 @@
 返回[["1","2","3"],["12","3"],["1","23"]]'''
 
 import copy
-class Solution:
-    """
-    @param: : a string to be split
-    @return: all possible split string array
-    """
 
-    def splitString(self, s):
-        # write your code here
+
+def test_i(n):
+    def printi(n,i):
+        for idx in range(i,n):
+            print(idx)
+            printi(n,idx+1)
+            print('pop')
+    printi(n,0)
+
+test_i(3)
+
+'''the important thing is,1st figiure out how to draw the tree,2nd,how many options
+for the combination:for i in range(len(nums)),or for i in range(2)'''
+
+class cutStringSolution:
+    def subsets(self,s):
         ret=[]
-        sp_list=[x for x in s]
-        ret.append(sp_list)
-        for idx,ch in enumerate(sp_list):
-            if idx+1 == len(sp_list):
-                break
-            cmb_str = ch+sp_list[idx+1]
-            temp_list = copy.deepcopy(sp_list)
-            temp_list[idx+1]=cmb_str
-            temp_list.pop(idx)
-            ret.append(temp_list)
-
+        self.dfs(ret,[],s)
         return ret
+
+    def dfs(self,result,path,s):
+        if s=='':
+            result.append(path[:])
+            return
+
+        for i in range(2):
+            if i<=len(s[:i+1])-1:
+                path.append(s[:i+1])
+                self.dfs(result,path,s[i+1:])
+                path.pop()
+
+
+s=cutStringSolution()
+print(s.subsets('1234'))
