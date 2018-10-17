e5324ff8413a12aa0e3a5476a9437b53c608936a
Sherlock-szx <sherlockatszx@qq.com>
2018-10-17 17:21:30 +0800

    find out bug reason
    explain why solution2 is wrong

--- print_binary.py
+++ print_binary.py
@@ -1,12 +1,13 @@
 class Solution:
     '''print binary'''
     def prt_bi(self,n):
-        self.ret=[]
+        self.ret=''
         self.dfs(n,'')
         return self.ret
     def dfs(self,i,prefix):
         if i==0:
-            self.ret.append(prefix)
+            print('i equals 0,the prefix is',prefix)
+            self.ret=self.ret+prefix
 
             return
 
@@ -36,3 +37,26 @@ s=Solution()
 import pdb
 pdb.set_trace()
 print(s.prt_bi(2))
+
+
+'''Why Solution2 is wrong
+
+During the recursion, last call first return,and first call last return,
+in Solution2,the final return is a recursion call,not the base case,so
+the return is None
+The scripts below demonstrate why'''
+
+t = {}
+
+def ex(x):
+    global t
+    if x > 1:
+        x -= 1
+        t[x] = ex(x)
+    else:
+        return x
+
+ex(10)
+print(t)
+
+'''the output is {1: 1, 2: None, 3: None, 4: None, 5: None, 6: None, 7: None, 8: None, 9: None}'''
