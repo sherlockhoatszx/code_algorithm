2afc619940a1ce3edb8fe47ff748eade7e46c401
Sherlock-szx <sherlockatszx@qq.com>
2018-10-23 21:39:16 +0800

    copy inside
    attention in dfs the temp array should copy before append to results

---
+++ permutation.py
@@ -0,0 +1,67 @@
+class Solution:
+    """
+    @param: nums: A list of integers.
+    @return: A list of permutations.
+    """
+    def permute(self, nums):
+
+        if nums is None:
+            return
+
+        results=[]
+        permutation=[]
+        visited=[0]*len(nums)
+
+        self.dfs(nums,results,permutation,visited)
+        return results
+
+    def dfs(self,nums,results,permutations,visited):
+
+        if len(nums)==len(permutations):
+            #attention, permutation copy here
+            permutations=permutations[:]
+            results.append(permutations)
+            return
+
+        for i in range(0,len(nums)):
+
+            if visited[i]==1:
+                continue
+
+            permutations.append(nums[i])
+
+            visited[i]=1
+
+            self.dfs(nums,results,permutations,visited)
+
+            visited[i]=0
+            permutations.pop()
+
+
+s=Solution()
+print(s.permute([1,2,3]))
+
+
+class Solution2:
+    """
+    @param nums: A list of Integers.
+    @return: A list of permutations.
+    """
+    def permute(self, nums):
+        # 
+        def _permute(result, temp, nums):
+            if nums == []:
+                result += [temp]
+            else:
+                for i in range(len(nums)):
+                    _permute(result, temp + [nums[i]], nums[:i] + nums[i+1:])
+
+        if nums is None:
+            return []
+
+        if nums is []:
+            return [[]]
+
+        result = []
+        _permute(result, [], sorted(nums))
+        return result
