9409e0aa599bf13b3f34f36ebcee6e9a9417e8d1
Sherlock-szx <sherlockatszx@qq.com>
2018-12-01 21:26:05 +0800

    heap
    heap

--- meeting_room2.py
+++ meeting_room2.py
@@ -25,3 +25,23 @@ class Solution:
             meeting_rooms = max(meeting_rooms, ongoing_meetings)
 
         return meeting_rooms
+
+    def minMeetingRooms(self,intervals):
+
+        """
+        first sort by the start time
+        then maintain the min_heap of the end time
+        """
+        if len(intervals) == 0:
+            return 0
+        intervals = sorted(intervals, key = lambda x: x.start)
+        import heapq
+        heap = []
+        heapq.heappush(heap, intervals[0].end)
+        for i in range(1, len(intervals)):
+            if intervals[i].start < heap[0]:
+                heapq.heappush(heap, intervals[i].end)
+            else:
+                heapq.heappop(heap)
+                heapq.heappush(heap, intervals[i].end)
+        return len(heap)
