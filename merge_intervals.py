f587492ec7c03e63008bb89b19b5b28d0d934d58
Sherlock-szx <sherlockatszx@qq.com>
2018-12-02 11:15:19 +0800

    Update merge_intervals.py
    compare pre.end curr.start

---
+++ merge_intervals.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @param intervals, a list of Interval
+    # @return a list of Interval
+    def merge(self, intervals):
+        intervals = sorted(intervals, key=lambda x: x.start)
+        result = []
+        for interval in intervals:
+            if len(result) == 0 or result[-1].end < interval.start:
+                result.append(interval)
+            else:
+                result[-1].end = max(result[-1].end, interval.end)
+        return result
