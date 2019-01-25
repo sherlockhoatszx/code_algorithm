483453dc01245a8f15a1b1cae4b6f6a0e4f0d54f
Sherlock-szx <sherlockatszx@qq.com>
2019-01-25 15:54:35 +0800

    Create merge_account.py
    build the function logic structure

---
+++ merge_account.py
@@ -0,0 +1,29 @@
+'''首先构造email_to_ids'email_to_ids':{'email1':[0,2],'email2':[2]}
+然后用union_find把相联通的列表下表更新他对应的老大哥
+step3,把每个联通图用老大哥的id 作key和邮件list作value,构建id_to_email_set
+step4,然后将老大哥的下表对应到人名,遍历添加merged_accounts中'''
+class UnionFind:
+
+    def initialize(self,accounts):
+
+    def find(self,id):
+        pass
+
+    def uion(self,id1,id2):
+        pass
+
+
+
+
+
+class Solution:
+
+    def accountMerge(self,accounts):
+        pass
+
+
+    def get_email_to_ids(self,accounts):
+        pass
+
+    def get_id_to_email_set(self,accounts):
+        pass
